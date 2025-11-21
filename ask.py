018a0638b322e71b5af6f09c8584ad610f8af8d9
miqqie <awmeiqin@gmail.com>
2025-11-21 18:58:52 +0800

    Update ask.py

--- ask.py
+++ ask.py
@@ -3,7 +3,7 @@ import os
 import streamlit as st
 import pandas as pd
 from openai import OpenAI
-from pandasai.agent import Agent
+from pandasai import Agent
 from pandasai.llm import OpenAI as PandasAIOpenAI
 from streamlit_mic_recorder import mic_recorder
 import tempfile
@@ -415,3 +415,4 @@ if st.session_state["input_mode"] == "Text":
     if prompt := st.chat_input("Enter your question..."):
 
         process_user_prompt(prompt, all_data_frames, current_response_mode, current_narration_mode)
+
